5c3dc001d854f02a9019d2c783d65681be4127f7
Matheus Monteiro <monteiro@desktop-home.home>
2023-07-18 14:00:53 -0300

    refactor - alterada estilizacao com remodelamento de css

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import styled from "styled-components";
 import "./App.css";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
@@ -8,16 +7,10 @@ import { InfoTable } from "./components/infoTable";
 
 function App() {
   return (
-    <>
-      <Container>
-        <InfoTable />
-      </Container>
-    </>
+    <div className="container">
+      <InfoTable />
+    </div>
   );
 }
-const Container = styled.div`
-  max-width: 1024px;
-  margin: 0px auto;
-  height: 100vh;
-`;
+
 export default App;
